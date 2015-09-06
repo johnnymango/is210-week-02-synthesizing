0f4cf15714f7168ee09ab2e211329d123692f767
Johnny Rodriguez <johnnydrodriguez@gmail.com>
2015-09-06 00:03:57 +0000

    This is the corrected file after running pylint & nosetest

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is a test.  This is only a test"""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
